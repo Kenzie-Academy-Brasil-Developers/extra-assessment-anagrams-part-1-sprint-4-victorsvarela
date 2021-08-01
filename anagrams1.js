d9995221d6d1cb417f003b18a2c482ace89cdc85
Victor Varela <vitaotapioqueiro@gmail.com>
2021-08-01 15:50:51 -0300

    subindo funcional, problemas apenas em limpar a tela

---
+++ anagrams1.js
@@ -0,0 +1,37 @@
+const button = document.getElementById("findButton");
+const result = document.getElementById("guarda_result")
+
+
+let anagrama = []
+
+const limpaTela = () => {
+    
+    result.innerText = ''
+    
+}
+
+const getAnagramsOf = (typedText) => {
+    let newTypedText = alphabetize(typedText)
+    
+    for (let i in palavras){
+        if (newTypedText === alphabetize(palavras[i]))
+        anagrama.push(palavras[i])
+    }
+    
+    result.innerText = anagrama.join(', ')
+    
+}
+
+
+button.addEventListener("click", function () {
+
+    limpaTela()
+    
+    let typedText = document.getElementById("input").value;
+    getAnagramsOf(typedText);
+});
+
+
+function alphabetize(a) {
+    return a.toLowerCase().split("").sort().join("").trim();
+}
